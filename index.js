583004eff254f65661f27ee616efc6c389de6b50
diabro22 <devinroberts2001@gmail.com>
2023-03-09 17:38:39 -0500

    Generate random string for rock paper scissors computer input

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-<script>
+     /*user inputs rock, paper or scissors*/
+    /*generate random choice for rock, paper or scissors*/
+    /*if user input is rock and computer input is scissors, display message 'you win'*/
+    /*if user input is rock and computer input is rock, display message 'it's a tie'*/
+    /*if user input is rock and computer input is paper, display message 'you lose'*/
+    /*if user input is paper and computer input is rock, display message 'you win'*/
+    /*if user input is paper and computer input is paper, display message 'it's a tie'*/
+    /*if user input is paper and computer input is scissors, display message 'you lose'*/
+    /*if user input is scissors and computer input is paper, display message 'you win'*/
+    /*if user input is scissors and computer input is scissors, dispay message 'it's a tie'*/
+    /*if user input is scissors and computer input is rock, display message 'you win'*/
     
-</script>
+    var textArray = [
+        'rock',
+        'paper',
+        'scissors'
+    ];
+
+    var randomString = Math.floor(Math.random() * textArray.length);
+    var callRandomString = textArray[randomString];
+
+    function getComputerChoice() {
+        console.log(callRandomString);  
+    }
+
+    getComputerChoice();
