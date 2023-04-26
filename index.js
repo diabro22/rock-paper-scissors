d7807425f0c578107857178fc474c0f420c12ded
diabro22 <devinroberts2001@gmail.com>
2023-04-26 15:17:20 -0400

    Create user choice and computer choice functions and begin playRound function build

--- index.js
+++ index.js
@@ -10,4 +10,34 @@
     /*if user input is scissors and computer input is scissors, dispay message 'it's a tie'*/
     /*if user input is scissors and computer input is rock, display message 'you win'*/
 
+    /*Define Functions*/
     
+    function getComputerChoice () {
+        const gameArray = ['Rock', 'Paper', 'Scissors'];
+
+        const random = Math.floor(Math.random() * gameArray.length);
+        return (random, gameArray[random]);
+    }
+
+    const playerSelection = 'Rock';
+    const computerSelection = getComputerChoice();
+
+    function playRound (playerSelection, computerSelection) {
+        if ((playerSelection == 'rock' && computerSelection =='scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper'))
+            {console.log('You Win!')}
+        else {console.log('Placeholder')};
+    }
+
+    /*Execution*/
+    getComputerChoice();
+    console.log(playerSelection);
+
+
+    /*Bugs:
+        getComputerChoice returns string # along with accompanying text*/
+
+    /*Updates for build:
+        Make user and computer input case insensitive
+        */
